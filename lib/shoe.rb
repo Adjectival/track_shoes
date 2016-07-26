ec00e0e00e4dd31fe3fa65220a3322824ba50b28
Alexander Jacks <alexd.jacks@gmail.com>
2016-07-26 09:21:53 -0700

    removes buggy validation

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   before_save(:capitalize_brand)
-  validates(:brand, {:presence => true, :uniqueness => true, :length => {:maximum => 60})
+  validates(:brand, {:presence => true, :length => {:maximum => 60}})
 
    private
     define_method(:capitalize_brand) do
